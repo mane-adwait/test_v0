8a4cfca18990906bd6536f13eab87c95121afe58
Adwait Mane <35707572+mane-adwait@users.noreply.github.com>
2024-10-14 15:18:53 -0400

    Update plot_v0.m.

--- plot_v0.m
+++ plot_v0.m
@@ -4,18 +4,36 @@
 load("mane_pd_v0_ws.mat")
 
 figure()
+
+subplot(2,1,1)
 hold on
 plot(motor_data.time, motor_data.motor_pos(:,1), '-r');
 title("Knee Motor Encoder"); xlabel("Time (s)"); ylabel("Position (rev)")
 grid on; xlim([0 0.5]);
 
-figure()
+subplot(2,1,2)
 hold on
 plot(motor_data.time, motor_data.motor_trq(:,1), '-r');
 plot(motor_data.time, motor_data.motor_trq_cmd(:,1), '--k');
 title("Knee Motor Torque"); xlabel("Time (s)"); ylabel("Torque (N*m)")
 legend(["Measured","Commanded"])
 grid on; xlim([0 0.5]);
+
+figure()
+
+subplot(2,1,1)
+hold on
+plot(motor_data.time, motor_data.motor_pos(:,1), '-r');
+title("Knee Motor Encoder"); xlabel("Time (s)"); ylabel("Position (rev)")
+grid on;
+
+subplot(2,1,2)
+hold on
+plot(motor_data.time, motor_data.motor_trq(:,1), '-r');
+plot(motor_data.time, motor_data.motor_trq_cmd(:,1), '--k');
+title("Knee Motor Torque"); xlabel("Time (s)"); ylabel("Torque (N*m)")
+legend(["Measured","Commanded"])
+grid on;
 
 % figure()
 % hold on
