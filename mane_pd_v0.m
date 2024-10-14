b08932cde9b59b46ca3b9891ed170af17cd16754
Adwait Mane <35707572+mane-adwait@users.noreply.github.com>
2024-10-14 15:00:30 -0400

    Update mane_pd_v0.m.

--- mane_pd_v0.m
+++ mane_pd_v0.m
@@ -11,11 +11,18 @@ addpath('share');
 % stride_frequency = 0.5; % Hz
 % num_strides = 5;
 % duration = num_strides/stride_frequency; % Seconds.
-duration = 4; % Seconds.
+freq = 50; % Hz. Update frequency.
+dt = 1/freq; % Seconds. 1 / (50 Hz).
+duration = 2*pi; % Seconds.
+num_intervals = floor(duration/dt);
+
+% Torque units = N*m. Peak torque = 1.7 N*m.
+cmd_time = linspace(0,duration,num_intervals);
+cmd_torque_knee = 0.1*sin(cmd_time); % cmd_time/scale_factor. Use a scale_factor > 1 to compress.
+torque_hip = 0;
 
 boom = BoomController(); % Connect to the boom ROS network.
-% boom.Legs.setKinematicsType(1,  rr-2d?); % Not required for a single
-% motor.
+% boom.Legs.setKinematicsType(1,  rr-2d?); % Not required for a single motor.
 boom.ready(); % Put the motors in control mode.
 
 % boom.Motors.setPGains() % Scalar input sets all motor gains. Vector input
@@ -29,13 +36,14 @@ boom.Motors.setControlModes(1); % Torque mode.
 
 boom.startRecording(floor(duration * 50)); % To preallocate.
 
-% Torque units = N*m. Peak torque = 1.7 N*m.
-torque_knee = -0.1; torque_hip = 0; % Hip motor.
+for k = 1:num_intervals
+    boom.Motors.setTorques([cmd_torque_knee(k) torque_hip]);
+    pauseSafe(boom, dt); % Pressing Ctrl+C switches to idle mode. Otherwise same as pause. 
+end
+
+torque_knee = 0; torque_hip = 0; % Hip motor.
 boom.Motors.setTorques([torque_knee torque_hip]);
-
-pauseSafe(boom, duration + 1.0); % Pressing Ctrl+C switches to idle mode. Otherwise same as pause. 
-
-% Replace the above with a PD control loop here.
+pauseSafe(boom, 0.5); % Pressing Ctrl+C switches to idle mode. Otherwise same as pause.
 
 boom.stopRecording();
 
@@ -53,4 +61,3 @@ leg_data = parseLegData(boom.LegData);
 % Encoder values: motor_data.motor_pos(:,motor_num);
 
 save('mane_pd_v0_ws.mat')
-
